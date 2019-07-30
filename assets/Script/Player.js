e52423179dca8259a0f56670772c969cacf66d13
halozxc <824927872@qq.com>
2019-07-30 19:55:31 +0800

    新的shake函数
    抛出以及边界返回

--- assets/Script/Player.js
+++ assets/Script/Player.js
@@ -12,39 +12,61 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+      
     },
 shake:function(){
 var down=cc.rotateTo(0.5,0).easing(cc.easeSineIn());
-var lu=cc.rotateBy(0.5,60).easing(cc.easeSineOut());
-var ru=cc.rotateBy(0.5,-60).easing(cc.easeSineOut());
+var lu=cc.rotateTo(0.5,60).easing(cc.easeSineOut());
+var ru=cc.rotateTo(0.5,-60).easing(cc.easeSineOut());
 return cc.repeatForever(cc.sequence(lu,down,ru,down));
 
 },
     // LIFE-CYCLE CALLBACKS:
-
+    onKeyDown(event){
+   if(event.keyCode==cc.macro.KEY.down)
+    {
+   cc.log('hhaha');
+   
+        this.hooking=true;//将状态转为射出
+   this.node.stopAllActions();
+  
+   var outh=cc.moveBy(2,cc.v2(-600*Math.tan(Math.PI/180*this.node.rotation),-600)).easing(cc.easeSineIn());
+ 
+   this.node.runAction(outh);   
+}
+     },
 
     // onLoad () {},
 onLoad(){
     this.node.runAction(this.shake());
+   // cc.log(typeof(cc.moveTo(0.5,6)));
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
 },
     start () {
 
     },
-
-    // update (dt) {},
+update(dt){
+if(Math.abs(this.node.x>467)||Math.abs(this.node.y)>437){
+    this.node.stopAllActions();
+    if(Math.abs(this.node.y<-437))
+    {
+        this.node.y=-437;
+    }
+    if(Math.abs(this.node.x>467))
+    {
+        this.node.x=467;
+    }
+    if(Math.abs(this.node.x<-467))
+    {
+        this.node.x=-467;
+    }
+    //this.node.runAction(cc.sequence(cc.moveTo(2,cc.v2(0,24)),this.shake()));
+    this.node.runAction(cc.moveTo(2,cc.v2(0,24)));
+    if(this.node.y==24&&this.node.x==0){
+        this.node.stopAllActions();
+        this.node.runAction(this.shake());
+    }
+}
+},
+    
 });
