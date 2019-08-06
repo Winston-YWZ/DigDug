7e501c4dbf8ad8daf00a2f5293deccea71c6656b
halozxc <824927872@qq.com>
2019-08-06 12:12:02 +0800

    hhh

---
+++ assets/Script/shop1/shopMain.js
@@ -0,0 +1,53 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.stone=cc.find("Canvas/stonebook2/state");
+        this.medicine=cc.find("Canvas/liquid/state");
+        this.leaf=cc.find("Canvas/reborn/state");
+        this.boom=cc.find("Canvas/explosive/state");
+        this.node.on(cc.Node.EventType.MOUSE_ENTER,this.onMouseEnter(event),this)
+    },
+    onMouseEnter(event){
+        cc.log("hhahah");
+        if(event.target==this.stone){
+            this.stone.opacity=255;
+        }
+    },
+    start () {
+
+    },
+
+
+    // update (dt) {},
+});
